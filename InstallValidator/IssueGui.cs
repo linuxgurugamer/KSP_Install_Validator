297e5b647371cec387a69b81611c38012b353f82
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-10-24 09:32:40 -0400

    Initial Commit

---
+++ InstallValidator/IssueGui.cs
@@ -0,0 +1,202 @@
+﻿
+// 
+//     Copyright (C) 2014 CYBUTEK
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+#region Using Directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+#endregion
+
+namespace InstallValidator
+{
+    public class IssueGui : MonoBehaviour
+    {
+        #region Fields        
+
+        private GUIStyle boxStyle;
+        private GUIStyle buttonStyle;
+        private bool hasCentred;
+        private GUIStyle labelStyle;
+        private GUIStyle messageStyle;
+        private GUIStyle nameLabelStyle;
+        private GUIStyle nameTitleStyle;
+        private Rect position = new Rect(Screen.width, Screen.height,0, 0);
+        private GUIStyle titleStyle;
+        //private bool isInitialised = false;
+
+
+        #endregion
+
+        #region Methods: protected
+
+        protected void Awake()
+        {
+            try
+            {
+                DontDestroyOnLoad(this);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+            Log.Info("IssueGui was created.");
+        }
+
+        protected void OnGUI()
+        {
+            try
+            {
+                this.position = GUILayout.Window(this.GetInstanceID(), this.position, this.Window, "KSP Installation Validation Monitor", HighLogic.Skin.window);
+                this.CentreWindow();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        protected void Start()
+        {
+            try
+            {
+                this.InitialiseStyles();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Methods: private
+
+        private void CentreWindow()
+        {
+            if (this.hasCentred || !(this.position.width > 0) || !(this.position.height > 0))
+            {
+                return;
+            }
+            this.position.center = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+            this.hasCentred = true;
+        }
+
+
+
+        
+        private void DrawCompatibilityIssues()
+        {
+            GUILayout.BeginVertical(this.boxStyle);
+            GUILayout.Label("INSTALLATION ISSUES", this.nameTitleStyle);
+            foreach (var error in Process.parseErrorMsgs)
+            {
+                Log.Error("IssueGui, message: " + error);
+                GUILayout.Label(error, messageStyle, GUILayout.MinWidth(575.0f));
+            }
+            GUILayout.EndVertical();
+        }
+
+
+
+        private void InitialiseStyles()
+        {
+            //if (Configuration.UseKspSkin)
+            //{
+            //    GUI.skin = HighLogic.Skin;
+            //}
+            this.boxStyle = new GUIStyle(HighLogic.Skin.box)
+            {
+                padding = new RectOffset(10, 10, 5, 5)
+            };
+
+            this.nameTitleStyle = new GUIStyle(HighLogic.Skin.label)
+            {
+                normal =
+                {
+                    textColor = Color.white
+                },
+                alignment = TextAnchor.MiddleLeft,
+                fontStyle = FontStyle.Bold,
+                stretchWidth = true
+            };
+
+            this.titleStyle = new GUIStyle(HighLogic.Skin.label)
+            {
+                normal =
+                {
+                    textColor = Color.white
+                },
+                alignment = TextAnchor.MiddleCenter,
+                fontStyle = FontStyle.Bold
+            };
+
+            this.nameLabelStyle = new GUIStyle(HighLogic.Skin.label)
+            {
+                fixedHeight = 25.0f,
+                alignment = TextAnchor.MiddleLeft,
+                stretchWidth = true
+            };
+
+            this.labelStyle = new GUIStyle(HighLogic.Skin.label)
+            {
+                fixedHeight = 25.0f,
+                alignment = TextAnchor.MiddleCenter,
+            };
+
+            this.messageStyle = new GUIStyle(HighLogic.Skin.label)
+            {
+                stretchWidth = true
+            };
+
+            this.buttonStyle = new GUIStyle(HighLogic.Skin.button)
+            {
+                normal =
+                {
+                    textColor = Color.white
+                }
+            };
+
+            //isInitialised = true;
+        }
+
+        private void Window(int id)
+        {
+            try
+            {
+                if (Process.parseErrorMsgs != null && Process.parseErrorMsgs.Count > 0)
+                {
+                    this.DrawCompatibilityIssues();
+                }
+                if (GUILayout.Button("CLOSE", this.buttonStyle))
+                {
+                    Destroy(this);
+                }
+                GUI.DragWindow();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ InstallValidator/Process.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace InstallValidator
+{
+    public class InstallLocInfo
+    {
+        public string Name = null;
+        public string FileName = null;
+        public string Directory = null;
+        public string Path = null;
+        public string Message = null;
+
+        public InstallLocInfo(object obj)
+        {
+            this.ParseJson(obj);
+        }
+
+        private void ParseJson( object obj)
+        {
+            var data = obj as Dictionary<string, object>;
+            if (data == null)
+            {
+                Process.ParseError = true;
+                Process.AddParseErrorMsg = "[InstallValidator] No data in dictionary (ParseJson)";
+                Log.Error("No data in dictionary (ParseJson)");
+                throw new ArgumentException("[InstallValidator] No data in dictionary (ParseJson)");
+            }
+
+            foreach (var key in data.Keys)
+            {
+                switch (key)
+                {
+                    case "NAME":
+                        this.Name =(string)data[key];
+                        Log.Info("NAME: " + Name);
+                        break;
+
+                    case "FILE":
+                        this.FileName = (string)data[key];
+                        Log.Info("FILE: " + FileName);
+                        break;
+
+                    case "DIRECTORY":
+                        this.Directory = (string)data[key];
+                        Log.Info("DIRECTORY: " + Directory);
+                        break;
+                    case "PATH":
+                        this.Path = (string)data[key];
+                        Log.Info("PATH: " + Path);
+                        break;
+
+                    case "MESSAGE":
+                        this.Message = (string)data[key];
+                        Log.Info("MESSAGE: " + Message);
+                        break;
+                }
+            }
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class Process: MonoBehaviour
+    {
+        internal static List<string> parseErrorMsgs = new List<string>();
+
+        //List<InstallLocInfo> installLocInfoList = new List<InstallLocInfo>();
+        public static bool ParseError { get; set; }
+        public static string AddParseErrorMsg { set { parseErrorMsgs.Add(value); } }
+
+        string ModName = "";    // This comes from the main NAME in the .version file
+
+        bool DoProcess(string fname)
+        {
+            string json = File.ReadAllText("GameData/" + fname);
+            var data = Json.Deserialize(json) as Dictionary<string, object>;
+            if (data == null)
+            {
+                ParseError = true;
+                AddParseErrorMsg = "[InstallValidator] Error in Json.Deserialize, file: " + fname;
+                Log.Error("[InstallValidator] Error in Json.Deserialize, file: " + fname);
+                return false;
+            }
+            
+            foreach (var key in data.Keys)
+            {
+                if (key == "NAME")
+                {
+                    ModName = (string)data[key];
+                }
+                if (key == "INSTALL_LOC" || key.StartsWith("INSTALL_LOC_"))
+                {
+                    var installLocInfo = new InstallLocInfo(data[key]);
+                    //installLocInfoList.Add(installLocInfo);
+                    ValidateInstallLoc(key, installLocInfo);
+                }
+            }
+            return true;
+        }
+
+        void ValidateInstallLoc(string stanza, InstallLocInfo ili)
+        {
+            string fullPath = "GameData/" + ili.Path;
+             if (ili.Path != null && !Directory.Exists(fullPath))
+            {
+                ParseError = true;
+                AddParseErrorMsg = ProcessMessage("Path", ili, stanza,
+                                        "<MODNAME> has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/<PATH>/<DIRECTORY>. Do not move any files from inside that folder.");
+
+                Log.Error("Missing path: " + ili.Path);
+                return;
+            }
+            fullPath += "/" + ili.Directory;
+            fullPath.Replace("//", "/");
+
+            if (ili.Directory != null && !Directory.Exists(fullPath))
+            {
+                ParseError = true;
+                AddParseErrorMsg = ProcessMessage("Directory", ili, stanza,
+                                        "<MODNAME> has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/<PATH>/<DIRECTORY>. Do not move any files from inside that folder.");
+
+                Log.Error("Missing directory: " + ili.Directory);
+                return;
+            }
+            fullPath += "/" + ili.FileName;           
+            fullPath.Replace("//", "/");
+            if (!File.Exists(fullPath))
+            {
+                ParseError = true;
+
+                AddParseErrorMsg = ProcessMessage("File", ili, stanza,
+                     "<MODNAME> has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/<PATH>/<DIRECTORY>. Do not move any files from inside that folder.");
+                Log.Error("Missing file: " + ili.FileName);
+                return;
+            }
+        }
+
+        string ProcessMessage(string fieldName, InstallLocInfo ili, string stanza, string defaultMsg)
+        {
+            if (ili.Message == null || ili.Message == "")
+                return defaultMsg;
+
+            if (ili.Name == null)
+                ili.Name = ModName;
+            string msg = ili.Message;
+
+            msg = msg.Replace("<MODNAME>", ili.Name);
+            msg = msg.Replace("<FILE>", ili.FileName);
+            msg = msg.Replace("<DIRECTORY>", ili.Directory);
+            msg = msg.Replace("<PATH>", ili.Path);
+            msg = msg.Replace("<STANZA>", stanza);
+            msg = msg.Replace("<FIELD>", fieldName);
+            msg.Replace("//", "/");
+            return msg;
+        }
+
+        public void Start()
+        {
+            Log.Info("[InstallValidator] Start");
+            DoProcess("InstallValidator/test.version");
+            if (ParseError)
+            {
+                gameObject.AddComponent<IssueGui>();
+            }
+        }
+    }
+}
